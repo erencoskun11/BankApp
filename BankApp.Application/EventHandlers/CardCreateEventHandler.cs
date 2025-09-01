1e1728285c1b6f5ea1e7963b219879c07b73c648
erencoskun11 <143544935+erencoskun11@users.noreply.github.com>
2025-09-01 18:17:05 +0300

    feat(card): update CardService and CardCreateEventHandler

--- BankApp.Application/EventHandlers/CardCreateEventHandler.cs
+++ BankApp.Application/EventHandlers/CardCreateEventHandler.cs
@@ -1,36 +1,25 @@
 ﻿using BankApp.Application.Etos;
-using BankApp.Application.Interfaces;
-using BankApp.Application.Services;
-using Chinchilla.Logging;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace BankApp.Application.EventHandlers
 {
     public class CardCreateEventHandler : IEventHandler<CardCreateEto>
     {
-
         private readonly ILogger<CardCreateEventHandler> _logger;
 
-        private readonly IElasticSearchService _elasticsearchService;
-
-        public CardCreateEventHandler(ILogger<CardCreateEventHandler> logger, IElasticSearchService elasticSearchService)
+        public CardCreateEventHandler(ILogger<CardCreateEventHandler> logger)
         {
             _logger = logger;
-            _elasticsearchService = elasticSearchService;
         }
 
-        public async Task HandleAsync(CardCreateEto @event)
+        public Task HandleAsync(CardCreateEto @event)
         {
-            _logger.LogInformation("CardCreateEto received. Indexing to ElasticSearch...");
-            await _elasticsearchService.IndexAsync(@event, "card-create-logs");
+            // Elasticsearch kaldırıldı, sadece log bırakıldı
+            _logger.LogInformation("CardCreateEto received. Card: {Card}", @event.MaskedCardNumber);
 
-            _logger.LogInformation("CardCreateEto indexed successfully. Card: {Card}", @event.MaskedCardNumber);
- 
+            return Task.CompletedTask;
         }
     }
 }
+
